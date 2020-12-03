ae1be7ea00964576ccefedbcb30a814393ab7e5f
nadiamaharani25 <75023363+nadiamaharani25@users.noreply.github.com>
2020-12-03 20:53:30 +0700

    menambahkan TUGAS 1.js

---
+++ LEVEL 1-TUGAS 1.js
@@ -0,0 +1,45 @@
+//String
+var nama='Nadia Maharani \n';
+
+//Number
+var umur=24;
+  
+//String
+var alamat='\n Jln.Urip Sumoharjo Blok Zeni, Palembang \n';
+
+//Array
+var hobi=['Desain', 'Menulis', 'Ngoding', 'Bermain Basket', 'Traveling'];
+  
+  
+//Object
+var asal={
+  univ:'UIN Raden Fatah Palembang',
+  jurusan:'S1 Sistem Informasi'
+};
+
+//String
+var kenapaTertarikProgramming='\n Dulu saya tidak suka ngoding. Namun semakin saya coba, saya semakin tertarik untuk mempelajarinya. Dan karena bagi saya ngoding itu seru dan penuh tantangan, juga bisa mempelajari banyak hal baru. Salah satunya dengan belajar pemrograman, kita bisa membuat suatu aplikasi yang bermanfaat. \n';
+
+//Array
+var yangSudahDipelajari=['HTML', 'CSS', 'PHP', 'Database MySQL'];
+
+//Boolean
+var pernahKesulitan=true;
+
+//String
+var alasanIkutBootcamp='\n Karena saya ingin belajar lebih banyak lagi soal programming dan menambah relasi pertemanan. Dan saya berharap lewat Bootcamp Arkademy, saya bisa memberikan impact yang positif untuk banyak orang melalui ilmu yang sudah di dapat nantinya. Dan saya ingin bisa membuat suatu aplikasi yang bermanfaat untuk banyak orang';
+
+//String
+var alasanTerimaBootcamp='\n Alasan mengapa Bootcamp Arkademy perlu menerima saya karena saya adalah seseorang yang mau berusaha untuk terus belajar. Saya menyukai hal baru dan bertemu dengan orang-orang baru, suka hal yang menantang (untuk dipelajari), meski sebenarnya saya adalah seseorang yang tidak begitu pandai soal programming.\n\nSaya juga selalu berusaha untuk belajar mandiri dengan mencari referensi belajar dari berbagai macam platform online ataupun youtube sebagai tambahan. Jika diberi kesempatan, saya ingin belajar dan membangun karya bersama dengan Arkademy.';
+
+
+console.log(nama);
+console.log(umur);
+console.log(alamat);
+console.log(hobi);
+console.log(`\n Asal saya dari ${asal.univ}, jurusan ${asal.jurusan}.`);
+console.log(kenapaTertarikProgramming);
+console.log(yangSudahDipelajari);
+console.log(pernahKesulitan);
+console.log(alasanIkutBootcamp);
+console.log(alasanTerimaBootcamp);
